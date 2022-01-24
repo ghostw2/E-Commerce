976652c3a56ff7714273b7735d6925c5107e407b
Menri <m.bardhi@wisch4web.de>
2022-01-24 16:36:33 +0100

    Repos in the final version

--- src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -1,13 +1,12 @@
 package com.example.ecommerce.repository;
 
+import com.example.ecommerce.model.Order;
 import com.example.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByEmail(String email);
-    List<User> findAll();
+public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findAllByUserOrderByCreatedDateDesc(User user);
 }
--- src/main/java/com/example/ecommerce/repository/TokenRepository.java
+++ src/main/java/com/example/ecommerce/repository/TokenRepository.java
@@ -3,7 +3,9 @@ package com.example.ecommerce.repository;
 import com.example.ecommerce.model.AuthenticationToken;
 import com.example.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TokenRepository extends JpaRepository<AuthenticationToken,Long> {
     AuthenticationToken findByUser(User user);
     AuthenticationToken findByToken(String token);
